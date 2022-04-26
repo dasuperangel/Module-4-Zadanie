1e23f8b4722ad11e269dcc1ec31118797e1c5363
dasuperangel <dasuperangel@gmail.com>
2022-04-26 18:33:09 +0300

    Добавьте файлы проекта.

---
+++ Module 4 Zadanie/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4_Zadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+            for (int k = 0; k < 3; k++)
+
+            {
+
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+
+                User.LastName = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+
+                User.Login = Console.ReadLine();
+
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+
+                var result = Console.ReadLine();
+
+                if (result == "Да")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+
+                User.Age = double.Parse(Console.ReadLine());
+
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+                Console.WriteLine("Спасибо за участие в опросе!");
+            }                    
+        }
+    }
+}
